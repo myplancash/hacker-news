2dcf5492f610456706fdf2cd769c420302d906aa
Sergio Esteban Torres <myplancash@gmail.com>
2023-06-23 11:14:16 -0500

    added new section so people can like posts

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import moment from 'moment';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const PostCard = ({ post }) => {
   const { author, story_title, story_url, created_at} = post;
-
+  console.log(created_at)
   const handleCardClick = () => {
     if(story_url) {
       window.open(story_url, '_blank');
@@ -22,13 +22,18 @@ const PostCard = ({ post }) => {
   };
 
   return (
-    <section className='card' onClick={handleCardClick} >
-      <p>{story_title}</p>
-      <p>
-         <FontAwesomeIcon icon={faClock} className="clock-icon" />
-        {`${getTimeAgo()} ago by ${author}`}
-      </p>
-    </section> 
+    <section className='card'>
+      <section className='card-title' onClick={handleCardClick} >
+          <p>
+            <FontAwesomeIcon icon={faClock} className="clock-icon" />
+            {`${getTimeAgo()} ago by ${author}`}
+          </p>
+          <p>{story_title}</p>
+      </section> 
+      <section className='card-likes'>
+          <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+      </section>
+    </section>
   )
 }
 
